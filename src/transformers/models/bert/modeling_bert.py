06d4ba1b9c91ca558ea75ea91b63325f23c1cf65
Mozhdeh Gheini <gheini72@gmail.com>
2021-11-15 22:07:49 -0800

    Follow updated add_code_sample_docstrings signature

--- src/transformers/models/bert/modeling_bert.py
+++ src/transformers/models/bert/modeling_bert.py
@@ -1790,7 +1790,7 @@ class BertForTokenClassificationWithPrefix(BertPreTrainedModel):
 
     @add_start_docstrings_to_model_forward(BERT_INPUTS_DOCSTRING.format("batch_size, sequence_length"))
     @add_code_sample_docstrings(
-        tokenizer_class=_TOKENIZER_FOR_DOC,
+        processor_class=_TOKENIZER_FOR_DOC,
         checkpoint=_CHECKPOINT_FOR_DOC,
         output_type=TokenClassifierOutput,
         config_class=_CONFIG_FOR_DOC,
